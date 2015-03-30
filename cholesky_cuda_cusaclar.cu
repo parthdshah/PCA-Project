286cb8ffd54dc0e16b1d22ae6d84ea518618c49b
yath90 <yathindrak@gmail.com>
2015-03-30 16:25:41 -0400

    Update cholesky_cuda_cusaclar.cu

--- cholesky_cuda_cusaclar.cu
+++ cholesky_cuda_cusaclar.cu
@@ -6,9 +6,9 @@
 
 // link to the documentation of the library - http://docs.nvidia.com/cuda/cusolver/index.html#axzz3V3SakC7i
 
-// author: yathindra kota 
-// mail: yatkota@ufl.edu
-// last modified: 21st, March, 2015
+// author	: yathindra kota 
+// mail		: yatkota@ufl.edu
+// last modified: 29 March, 2015
 
 #include <stdio.h>
 #include <time.h>
@@ -20,12 +20,8 @@
 #include <cublas_v2.h>
 #include <cudense.h>
 
-#define num_rows 100
-#define num_cols 100
-
 #define NUM_THREADS (num_rows*num_cols)
 #define BLOCK_WIDTH 1000
-
 
 void check_error(const char *msg)
 {
@@ -41,118 +37,129 @@ void check_error(const char *msg)
 int main(int argc, char* argv[])
 { 
 	cudsHandle_t cudenseH = NULL;
-	cublasHandle_t cublasH = NULL;
-    	cublasStatus_t cublas_status = CUBLAS_STATUS_SUCCESS;
     	cusolverStatus_t cusolver_status = CUSOLVER_STATUS_SUCCESS;  
+	
 	int lwork = 0;
 	int *devInfo = NULL;
+	int info_gpu_h = 0;
+	int num_rows = 4;
+	int num_cols = 4;
+	int num_iterations = 10;
 	
-	// allocating matrix on the host device
-	
-	double h_matrix[num_rows*num_cols];
-	double h_matrix_ouput[num_rows*num_cols];
-	
-	//initialize the matrix to a random set of values (0 to 10) of type float 	
-	for(int tempi=0; tempi<num_rows;tempi++)
-	{
-		for(int tempj=0; tempi<num_cols;tempj++)	
-		{
-			h_matrix[ (tempi*num_rows) + tempj] = (rand()%100)/10; // set the input matrix elements 
-		}	
-	}		
-
-	// create cudense/cublas handle
-    	cusolver_status = cudsCreate(&cudenseH);
-    	assert(CUSOLVER_STATUS_SUCCESS == cusolver_status);
-
-    	cublas_status = cublasCreate(&cublasH);
-    	assert(CUBLAS_STATUS_SUCCESS == cublas_status);
-	
-	// allocate memory in the GPU and set the output matrix to 0s.. And also copy the input matrix from hot to device
-	
-	double *d_matrix_input, *d_matrix_output;
-    	check_error(cudaMalloc((void **) &d_matrix_input, num_rows * num_cols * sizeof(float)));
-	check_error(cudaMalloc((void **) &d_matrix_output, num_rows * num_cols * sizeof(float)));
-	check_error(cudaMemset(*d_matrix_output, 0, num_rows * num_cols * sizeof(float)));
-	check_error(cudaMemcpy(*d_matrix_input, *h_matrix, sizeof(float) * num_rows * num_cols, cudaMemcpyHostToDevice));
-
-	//------	
-	// alocation of the buffer in the GPU
-	// The following function allocates required amount of memory in the GPU and this
-	// function is specific to Cholesky function which is called next
-	// This function is also from the library cuSolver
-		
-	cusolverStatus_t cusolverDnDpotrf_bufferSize(cudenseH,
-                 NULL, /*cublasFillMode_t uplo, Maybe "CUBLAS_FILL_MODE_LOWER" */
-                 num_rows,
-                 d_matrix_input,
-                 num_rows,
-                 &Lwork);
-	
-	double *d_work = NULL; 	
-	cudaStat1 = cudaMalloc((void**)&d_work, sizeof(double)*lwork);
-    	assert(cudaSuccess == cudaStat1);
-	
-	timer.start();
-	
-	// implementation of Cholesky
-	cusolverStatus_t cusolverDnDpotrf(cudenseH,
-           NULL,/* not sure, needs to be checked*/
-           num_rows,
-           d_matrix_input,
-           num_rows,
-           d_work, /*not sure */
-           Lwork,
-           devInfo );
-	
-	cudaStat1 = cudaDeviceSynchronize(); // synchronization
-    	assert(CUSOLVER_STATUS_SUCCESS == cusolver_status);
-    	assert(cudaSuccess == cudaStat1);
-	
-	timer.stop();	
-	//// check if Cholesky is good or not
-
-	cudaStat1 = cudaMemcpy(&info_gpu, devInfo, sizeof(int), cudaMemcpyDeviceToHost);
-    	assert(cudaSuccess == cudaStat1);
-	
-	// following can be added if required
-    	//printf("after geqrf: info_gpu = %d\n", info_gpu); 
-    	assert(0 == info_gpu);
-
-	cudaStat1 = cudaMemcpy(h_matrix_ouput,d_work , sizeof(double)*num_rows*num_cols, cudaMemcpyDeviceToHost); 
-	//d_work needs to checked
-    	
-    	assert(cudaSuccess == cudaStat1);
-	
-	printf("Time elapsed = %g ms\n", timer.Elapsed());
-	
-	// print output to file
 	FILE *fp;
 	fp = fopen("cholesky_output.txt","w");
-	fprintf(fp,"output is\n");	
 	
+	// create cudense/cublas handle
+    	cusolver_status = cudsCreate(&cudenseH);
+    	cudaError_t cudaStat1;
+	float time_iterations = 0; //time for given set of iterations
+	
+	for(int matsize = 0; matsize < 10; matsize++)
+	{	
+		double *h_matrix;
+		double *h_matrix_ouput;
+		
+		h_matrix = malloc(num_rows*num_cols, sizeof(double));
+		h_matrix_ouput = malloc(num_rows*num_cols, sizeof(double));
+		
+		int temp;
+		for(temp = 0; temp < num_iterations; temp++)
+		{
+			//initialize the matrix to a random set of values (0 to 10) of type float 	
+			for(int tempi=0; tempi<num_rows;tempi++)
+			{
+				for(int tempj=0; tempi<num_cols;tempj++)	
+				{
+					h_matrix[(tempi*num_rows) + tempj] = (rand()%100)/10; // set the input matrix elements 
+				}	
+			}		
+
+			// allocate memory in the GPU and also copy the input matrix from hot to device
+			double *d_matrix_input;
+			check_error(cudaMalloc((void **) &d_matrix_input, num_rows * num_cols * sizeof(float)));
+			check_error(cudaMemcpy(*d_matrix_input, *h_matrix, sizeof(float) * num_rows * num_cols, cudaMemcpyHostToDevice));
+
+			//------	
+			// alocation of the buffer in the GPU
+			// The following function allocates required amount of memory in the GPU and this
+			// function is specific to Cholesky function which is called next
+			// This function is also from the library cuSolver
+				
+			cusolverStatus_t cusolverDnDpotrf_bufferSize(cudenseH,
+						 CUBLAS_FILL_MODE_LOWER, /*cublasFillMode_t uplo, Maybe "CUBLAS_FILL_MODE_LOWER" */
+						 num_rows,
+						 d_matrix_input,
+						 num_rows,
+						 &Lwork);
+			
+			double *d_work = NULL; 	
+			cudaStat1 = cudaMalloc((void**)&d_work, sizeof(double)*Lwork);
+			
+			cudaStat1 = cudaDeviceSynchronize(); // synchronization
+			
+			timer.start();
+			
+			// implementation of Cholesky
+			cusolverStatus_t cusolverDnDpotrf(cudenseH,
+				   CUBLAS_FILL_MODE_LOWER,/* not sure, needs to be checked*/
+				   num_rows,
+				   d_matrix_input,
+				   num_rows,
+				   d_work, /*not sure */
+				   Lwork,
+				   devInfo );
+			
+			cudaStat1 = cudaDeviceSynchronize(); // synchronization
+			
+			timer.stop();
+			
+			time_iterations += timer.Elapsed;	
+			
+			//// check if Cholesky is good or not
+			cudaStat1 = cudaMemcpy(&info_gpu_h, devInfo, sizeof(int), cudaMemcpyDeviceToHost);
+			
+			if(info_gpu_h != 0)
+			{
+				fprintf(fp,"Unsuccessful execution. DevInfo is not zero. Iterations number = %d \n", temp);
+			}	
+		}
+		cudaStat1 = cudaMemcpy(h_matrix_ouput, d_work, sizeof(double)*num_rows*num_cols, cudaMemcpyDeviceToHost); 
+		//d_work needs to checked
+		
+		printf("Time elapsed(average for the specified iterations) = %g ms, number of rows is %d\n", time_iterations/num_iterations,num_rows);
+		
+		time_iterations = 0;
+		
+		free(h_matrix);
+		free(h_matrix_ouput);		
+			
+		num_cols *= 2;
+		num_rows *= 2;
+	}
+	// print output to file if required
+	/*
+	
+	fprintf(fp,"output is\n");	
 	for(tempi=0; tempi<num_rows;tempi++)
 	{
 		fprintf(fp,"\n");
 		for(tempj=0; tempi<num_cols;tempj++)	
 		{
 			fprintf(fp,"%f", h_matrix_ouput[(tempj * num_rows) + tempi]);
-		}	
-		
+		}		
 	}
-
+	
+	*/
 	//de-allocating the memory
+	
 	if(d_matrix_input)
 		cudaFree(d_matrix_input);
-		
-	if(d_matrix_output)
-		cudaFree(d_matrix_output);
 		
 	if(d_work)
 		cudaFree(d_work);
 	
-	if (cublasH ) cublasDestroy(cublasH);   
-    	if (cudenseH) cudsDestroy(cudenseH); 	
+    	if (cudenseH) 
+		cudsDestroy(cudenseH); 	
 
 	return 0;	
 }
